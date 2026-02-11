a5b209f9b872ba2b59149c1927b4941dcd7d5c69
BogdanMatran <bogdancristian.matran@gmail.com>
2026-02-11 15:25:47 +0100

    Update todo.rb

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -1,9 +1,9 @@
  class Todo < Formula                                                                                                                                                      
     desc "A simple CLI todo app built with Kotlin"
     homepage "https://github.com/BogdanMatran/todo-cli"                                                                                                                     
-    url "https://github.com/BogdanMatran/todo-cli/releases/download/v1.0/kotlinlearn-1.0-SNAPSHOT-all.jar"                                                                  
-    sha256 "37542415147ef6a7a62de77af5b5e47db1ace61b2774dbce1085dbf1f9ac08e4"
-    version "1.0"
+    url "https://github.com/BogdanMatran/todo-cli/releases/download/v1.1/kotlinlearn-1.0-SNAPSHOT-all.jar"                                                                  
+    sha256 "a54232c697425335e0e9286739bd410c969df49a9e6115bdd11d095e2ccda36e"
+    version "1.1"
 
     depends_on "openjdk"
 
